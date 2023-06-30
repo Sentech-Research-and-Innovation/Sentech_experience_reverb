913e9d4bb4aea032f0c08401e6f11374b6790181
Mandiso Ngwenya <mandiso@webgap.co.za>
2023-06-30 10:59:30 +0200

    sentiment analysis build

--- resources/js/Pages/Admin/SentimentAnalysis/Index.vue
+++ resources/js/Pages/Admin/SentimentAnalysis/Index.vue
@@ -16,16 +16,31 @@
             >
 
                 <template v-slot:item.user="{ item }">
-                    <a :href="'https://twitter.com/'+item.raw.user" target="_new"><i class="fa fa-twitter"> </i>@{{item.raw.user}}</a>
+                    <div class="twitter-username"><a :href="'https://twitter.com/'+item.raw.user" target="_new">@{{item.raw.user}}</a>
+                    </div>
                 </template>
                 <template v-slot:item.user_mentions="{ item }">
-                    <a v-if="item.raw.user_mentions" v-for="usr in item.raw.user_mentions" :href="'https://twitter.com/'+usr" target="_new"> @{{usr}} </a>
+                    <div class="row">
+                        <div v-if="item.raw.user_mentions" v-for="usr in item.raw.user_mentions"
+                             style="margin-top: 7px; margin-bottom: 7px">
+                            <div class="twitter-username "><a :href="'https://twitter.com/'+usr" target="_new">
+                                @{{usr}} </a></div>
+                        </div>
+                    </div>
+
+
+                </template>
+                <template v-slot:item.sentiment="{ item }">
+                    <div class="default-sentiment"
+                         :class="{'negative-sentiment': (item.raw.sentiment === 'negative'), 'positive-sentiment': (item.raw.sentiment === 'positive'), 'neutral-sentiment': (item.raw.sentiment === 'neutral')}">
+                        {{item.raw.sentiment}}
+                    </div>
                 </template>
                 <template v-slot:item.text="{ item }">
                     {{item.raw.text}}
                 </template>
                 <template v-slot:item.actions="{ item }">
-<!--                    {{item.raw}}-->
+                    <!--                    {{item.raw}}-->
                 </template>
                 <template v-slot:no-data>
                     <v-btn color="primary" @click="initialize">
@@ -34,8 +49,8 @@
                 </template>
             </v-data-table>
 
-<!--            <DataTable :datatable="datatable" :data="data" ></DataTable>-->
-<!--            <Pagination :links=" data.roles.links" :data=" data.roles"/>-->
+            <!--            <DataTable :datatable="datatable" :data="data" ></DataTable>-->
+            <!--            <Pagination :links=" data.roles.links" :data=" data.roles"/>-->
         </div>
     </div>
 </template>
@@ -44,14 +59,15 @@
     import AdminLayout from '@/Layouts/AdminLayout.vue';
     import DataTable from '../../Shared/DataTable.vue';
     import Pagination from '../../../Components/Pagination.vue';
+
     export default {
-        props:['data'],
-        components:{AdminLayout,DataTable,Pagination},
+        props: ['data'],
+        components: {AdminLayout, DataTable, Pagination},
         layout: AdminLayout,
         data: () => {
             return {
                 datatable: {
-                    modalComponent:'edit-role',
+                    modalComponent: 'edit-role',
                     headers: [
                         {
                             title: 'Tweet',
@@ -65,12 +81,14 @@
                             align: 'start',
                             sortable: true,
                             key: 'user',
+                            width: '5%'
                         },
                         {
                             title: 'User Mentions',
                             align: 'start',
                             sortable: true,
                             key: 'user_mentions',
+                            width: '5%'
                         },
 
                         {
@@ -78,6 +96,7 @@
                             align: 'start',
                             sortable: true,
                             key: 'sentiment',
+                            width: '5%'
                         },
                         {
                             title: 'Place',
@@ -98,7 +117,7 @@
                     modal: true,
                     view: true,
                     viewLink: false,
-                    formData:{
+                    formData: {
                         action: 'edit',
                         edit: true
                     },
@@ -111,12 +130,57 @@
             }
         },
         mounted() {
-            this.datatable.items =this.data.analysis
+            this.datatable.items = this.data.analysis
         }
 
     }
 </script>
 
-<style>
+<style scoped lang="scss">
+    .twitter-username a {
+        color: #fff;
+        text-decoration: none;
+        background: #144f9f;
+        padding: 5px;
+        border-radius: 8px;
+        margin: 2px;
+    }
+
+    .negative-sentiment {
+        color: #000 !important;
+        text-decoration: none;
+        background: #f00 !important;
+        padding: 5px;
+        border-radius: 8px;
+        margin: 2px;
+    }
+
+    .positive-sentiment {
+        color: #000 !important;
+        text-decoration: none;
+        background: #66FF00 !important;
+        padding: 5px;
+        border-radius: 8px;
+        margin: 2px;
+    }
+
+    .neutral-sentiment {
+        color: #000 !important;
+        text-decoration: none;
+        background: #d3d0c9 !important;
+        padding: 5px;
+        border-radius: 8px;
+        margin: 2px;
+    }
+
+    .default-sentiment {
+        font-weight: bold;
+        color: #fff;
+        text-decoration: none;
+        background: #144f9f;
+        padding: 10px;
+        border-radius: 8px;
+    }
+
 
 </style>
