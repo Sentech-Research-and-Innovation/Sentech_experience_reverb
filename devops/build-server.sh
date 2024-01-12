c8557f594c2e388bc40caf4af8d97b56f9134b6c
unknown <Lizwi>
2024-01-12 15:27:58 +0200

    added script for project

--- devops/build-server.sh
+++ devops/build-server.sh
@@ -9,8 +9,7 @@
 
 apt -qy update
 apt -qy install curl git zip unzip
-
-docker-php-ext-install pdo_mysql ctype bcmath zip
+docker-php-ext-install pdo_mysql ctype bcmath zip unzip
 
 # Install Composer
 curl --silent --show-error https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
