6b01634b94e7e215a007a5132e2b45fdf22403a4
Lizwi <Lizwi>
2024-07-22 14:44:44 +0200

    changes

--- public/build/assets/filter-2b343464.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./app-1f9c4a13.js";const r=t("filter",{state:()=>({date:null,keywords:null,sentimentTypes:null}),getters:{searchFilter:e=>({date:e.date,keywords:e.keywords,sentimentTypes:e.sentimentTypes})},actions:{},persist:!0});export{r as u};
---
+++ public/build/assets/networks-5f3a2b4b.js
@@ -0,0 +1,1 @@
+import{N as r}from"./app-0d314f6a.js";const i=r("selectdProvince",{state:()=>({province:""}),getters:{FilterProvince:e=>({province:e.province})},actions:{},persist:!0});export{i as u};
