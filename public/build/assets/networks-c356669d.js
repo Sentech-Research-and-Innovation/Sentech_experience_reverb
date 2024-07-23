059cce1811b6ef432d4ad6ccdf8604f47bcb1de0
Lizwi <Lizwi>
2024-07-23 11:52:14 +0200

    changes

--- public/build/assets/networks-c356669d.js
+++
@@ -1,1 +0,0 @@
-import{P as r}from"./app-484c75fe.js";const i=r("selectdProvince",{state:()=>({province:""}),getters:{FilterProvince:e=>({province:e.province})},actions:{},persist:!0});export{i as u};
