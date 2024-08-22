98ed1ab724be19b3274418f5555e5cde5c3be380
Lizwi <Lizwi>
2024-08-22 09:17:00 +0200

    changes

--- app/Http/Controllers/Admin/Reports/SentimentsReport.php
+++ app/Http/Controllers/Admin/Reports/SentimentsReport.php
@@ -69,7 +69,36 @@ class SentimentsReport extends Controller
 
             // $html = '<footer><div class="footer">My foorter</div></footer>';
 
-            $footerHtml =  view('reports/footer')->render();
+
+
+            $footerHtml =  '<style>
+    html {
+        -webkit-print-color-adjust: exact;
+    }
+
+    .footer {
+
+        color: #ffffff;
+        width: 100%;
+        background-color: #144f9f;
+        display: block;
+        font-size: 11px;
+        -webkit-print-color-adjust: exact;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        padding: 10px 10px;
+        text-align: center
+    }
+</style>
+
+
+
+<div class="footer">
+
+    www.sentech.co.za | Email: support@sentech.co.za | Call Centre: 0860 736 832
+
+</div>';
             $headerHtml =  view('reports/header')->render();
 
             $pdfStoredPath = PDF::loadView('reports/sentiments', compact('data'))->margins(10, 25, 17, 25)
