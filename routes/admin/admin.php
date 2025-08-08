300c5f31e4741ac54508ce95fc3575961aa2474c
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-08-08 12:37:27 +0200

    Update admin.php

--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -8,3 +8,17 @@ use App\Http\Controllers\Admin\GetNotifications;
 
 
 Route::get('/admin/notifications', [GetNotifications::class, 'index'])->middleware('auth');
+
+// Route::get('/admin/dashboard/stats', [DashboardController::class, 'getDashboardStats']);
+Route::get('/admin/dashboard/stats', function(){
+
+    return response()->json([
+            'pending_companies' => Company::count(),
+            'company_requests' => Company::count(), // or however you define this
+            'system_users' => User::count(),
+            // // // 'customer_feedback' => Feedback::count(), // adjust model if it's named differently
+            'customer_feedback' => User::count(),
+            // 'test' => 'Dashboard stats working!',
+        ]);
+    
+    });
