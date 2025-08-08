41f39ff964c7fdd2fb28d5eba238c33cc8bd0ab7
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-08-08 13:02:22 +0200

    Update admin.php

--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -5,23 +5,10 @@ use App\Http\Controllers\Web\WeatherController;
 use App\Http\Controllers\Admin\GetNotifications;
 use App\Http\Controllers\Admin\DashboardController;
 
-use App\Models\Company;
-use App\Models\User;
 
 
 
 Route::get('/admin/notifications', [GetNotifications::class, 'index'])->middleware('auth');
 
 Route::get('/admin/dashboard/stats', [DashboardController::class, 'getDashboardStats'])->middleware('auth');
-// Route::get('/admin/dashboard/stats', function(){
 
-//     return response()->json([
-//             'pending_companies' => Company::count(),
-//             'company_requests' => Company::count(), // or however you define this
-//             'system_users' => User::count(),
-//             // // // 'customer_feedback' => Feedback::count(), // adjust model if it's named differently
-//             'customer_feedback' => User::count(),
-//             // 'test' => 'Dashboard stats working!',
-//         ]);
-    
-//     });
