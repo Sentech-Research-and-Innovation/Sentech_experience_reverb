5f4db4c6c0210e31509628a802aa826069c83f1f
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-08-08 12:42:03 +0200

    Update admin.php

--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -12,16 +12,16 @@ use App\Models\User;
 
 Route::get('/admin/notifications', [GetNotifications::class, 'index'])->middleware('auth');
 
-// Route::get('/admin/dashboard/stats', [DashboardController::class, 'getDashboardStats']);
-Route::get('/admin/dashboard/stats', function(){
+Route::get('/dashboard/stats', [DashboardController::class, 'getDashboardStats'])->middleware('auth');
+// Route::get('/admin/dashboard/stats', function(){
 
-    return response()->json([
-            'pending_companies' => Company::count(),
-            'company_requests' => Company::count(), // or however you define this
-            'system_users' => User::count(),
-            // // // 'customer_feedback' => Feedback::count(), // adjust model if it's named differently
-            'customer_feedback' => User::count(),
-            // 'test' => 'Dashboard stats working!',
-        ]);
+//     return response()->json([
+//             'pending_companies' => Company::count(),
+//             'company_requests' => Company::count(), // or however you define this
+//             'system_users' => User::count(),
+//             // // // 'customer_feedback' => Feedback::count(), // adjust model if it's named differently
+//             'customer_feedback' => User::count(),
+//             // 'test' => 'Dashboard stats working!',
+//         ]);
     
-    });
+//     });
