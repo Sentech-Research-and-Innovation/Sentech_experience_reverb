97bcbdac3471d1be0fca1cfabcdd17a2fd3f82e0
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-08-10 16:23:35 +0200

    Update requestDeclineMail.php

--- app/Mail/requestDeclineMail.php
+++ app/Mail/requestDeclineMail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class RequestDeclinedMail extends Mailable
+class RequestDeclineMail extends Mailable
 {
     use Queueable, SerializesModels;
 
