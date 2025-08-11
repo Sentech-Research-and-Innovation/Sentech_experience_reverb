4c691ee1a3cb2915e25ac2e10c55f3097b5c2d55
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-08-11 17:51:45 +0200

    Update profile.php

--- routes/admin/profile.php
+++ routes/admin/profile.php
@@ -7,4 +7,4 @@ use  App\Http\Controllers\Profile\ProfileController;
 Route::get('/profile/index', [ProfileController::class, 'index']);
 Route::post('/profile/update', [ProfileController::class, 'update'])->middleware('auth');
 Route::post('/profile/update/password', [ProfileController::class, 'updatePassword']);
-Route::get('/profile/{id}', [ProfileController::class, 'show'])->name('admin.profile.show');
+Route::get('/profile/show/{id}', [ProfileController::class, 'show']);
