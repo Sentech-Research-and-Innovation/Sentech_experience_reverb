2566d2853544b1642b8034e9bae0723fcfdab5ae
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-08-20 09:39:18 +0200

    Update profile.php

--- routes/admin/profile.php
+++ routes/admin/profile.php
@@ -8,3 +8,5 @@ Route::get('/profile/index', [ProfileController::class, 'index']);
 Route::post('/profile/update', [ProfileController::class, 'update'])->middleware('auth');
 Route::post('/profile/update/password', [ProfileController::class, 'updatePassword']);
 Route::get('/profile/{id}', [ProfileController::class, 'show']);
+Route::post('/upload-profile-image', [ProfileController::class, 'uploadProfileImage']);
+Route::delete('/delete-profile-image', [ProfileController::class, 'deleteProfileImage']);
