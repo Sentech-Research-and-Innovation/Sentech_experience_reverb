946a6fb65be1e721b70b887f9c675330f641552c
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-08-28 15:25:25 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,6 @@ use LaravelAndVueJS\Traits\LaravelPermissionToVueJS;
 
 use App\Notifications\ResetPasswordNotification;
 
-use Illuminate\Support\Facades\Storage;
 
 
 class User extends Authenticatable
@@ -97,10 +96,10 @@ class User extends Authenticatable
         }
         
         // Generate the full URL to the image
-        return Storage::disk('public')->url($this->profile_photo_path);
+        // return Storage::disk('public')->url($this->profile_photo_path);
         
         // OR if you prefer using the asset helper:
-        // return asset('storage/' . $this->profile_photo_path);
+        return asset('storage/' . $this->profile_photo_path);
     }
 
 }
