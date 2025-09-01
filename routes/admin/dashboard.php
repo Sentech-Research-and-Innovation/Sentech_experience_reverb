9e23e67933aa4d19f536f70c33801dcf73a21415
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-09-01 14:00:51 +0200

    Update dashboard.php

--- routes/admin/dashboard.php
+++ routes/admin/dashboard.php
@@ -2,8 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\SenTalkController;
 
 
 
 Route::get('/admin/dashboard',  [DashboardController::class, 'index'])->middleware(['auth']);
 Route::post('/admin/activities',  [DashboardController::class, 'show'])->middleware(['auth']);
+
+Route::prefix('sentalk')->group(function () {
+    Route::get('/', [SenTalkController::class, 'index']);
+    Route::post('/upload', [SenTalkController::class, 'upload']);
+    Route::get('/{id}', [SenTalkController::class, 'show']);
+    Route::get('/download/{id}', [SenTalkController::class, 'download']);
+    Route::get('/stats', [SenTalkController::class, 'stats']);
+});
