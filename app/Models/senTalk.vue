d9160d91428cf57150f421302e36a6ab347cdb1c
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-09-01 13:49:09 +0200

    Create senTalk.vue

---
+++ app/Models/senTalk.vue
@@ -0,0 +1,11 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SenTalk extends Model
+{
+    protected $fillable = [
+        'pdf_path', 'title', 'creator',
+        'number_views', 'number_likes', 'number_downloads'
+    ];
+}
