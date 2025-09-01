b037cfdbf73736aa07be87bf13c60d97be54806c
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-09-01 12:35:21 +0200

    Update company.php

--- routes/admin/company.php
+++ routes/admin/company.php
@@ -5,6 +5,8 @@ use  App\Http\Controllers\Organizations\OrganizationsController;
 
 
 Route::get('/organizantions/approved', [OrganizationsController::class, 'approved'])->middleware('role_has_permission:companies-read_approved');
+Route::get('/organizantions/ActivityLog', [OrganizationsController::class, 'ActivityLog'])->middleware('role_has_permission:companies-read_approved');
+
 Route::get('/organizantions/request', [OrganizationsController::class, 'request'])->middleware('role_has_permission:companies-read_requests');
 Route::get('/organizantions/pending', [OrganizationsController::class, 'pending'])->middleware('role_has_permission:companies-read_pending');
 Route::post('/organizantions/create', [OrganizationsController::class, 'create'])->middleware('role_has_permission:companies-create_company');
