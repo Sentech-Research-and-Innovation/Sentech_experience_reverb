20d0822eb1a0a6f2ae5b9b56b9096eded82ecadb
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-09-02 14:49:15 +0200

    Update senTalk.vue

--- app/Models/senTalk.vue
+++ app/Models/senTalk.vue
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
 use Illuminate\Database\Eloquent\Model;
 
 class SenTalk extends Model
@@ -11,7 +10,10 @@ class SenTalk extends Model
     use HasFactory;
 
     protected $fillable = [
-        'pdf_path', 'title', 'creator',
-        'number_views', 'number_likes', 'number_downloads'
+        'title',
+        'pdf_path',
+        'number_views',
+        'number_downloads',
+        'number_likes',
     ];
 }
