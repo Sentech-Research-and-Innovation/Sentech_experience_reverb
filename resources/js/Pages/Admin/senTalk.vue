5d865ae20818f1c105cd8c9401ed213e5c9467e8
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-09-14 18:05:16 +0200

    Update senTalk.vue

--- resources/js/Pages/Admin/senTalk.vue
+++ resources/js/Pages/Admin/senTalk.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { Home, Smile, Wrench, Lock, Users, Radio } from "lucide-vue-next";
+</script>
+
 <template>
     <Head :title="'Dashboard'"><title>Dashboard</title></Head>
 
@@ -110,8 +114,7 @@
 import AdminLayout from "@/Layouts/AdminLayout.vue";
 import { defineComponent, onMounted, ref } from "vue";
 import { Head } from "@inertiajs/inertia-vue3";
-import senTalk from "./senTalk.vue";
-import { Home, Smile, Wrench, Lock, Users, Radio } from "lucide-vue-next";
+import senTalk from "./senTalk.vue";\
 
 export default defineComponent({
     name: "dashboard",
