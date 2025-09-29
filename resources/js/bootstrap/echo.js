131d6599faa8ca819740e22906196c6948926371
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-09-29 17:08:05 +0200

    Update echo.js

--- resources/js/bootstrap/echo.js
+++ resources/js/bootstrap/echo.js
@@ -1,22 +1,22 @@
 // echo.js
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
-import axios from 'axios';
+import axios from "axios";
 
 window.Pusher = Pusher;
 
 const echo = new Echo({
   broadcaster: "pusher",
   key: process.env.VUE_APP_PUSHER_KEY || "somekey",
-  wsHost: "13.247.190.223",     // your EC2 domain/IP
+  wsHost: "13.247.190.223", // your EC2 domain/IP
   wsPort: 6001,
-  forceTLS: false,              // true if using TLS and port 6001 over WSS
+  forceTLS: false,
   encrypted: false,
   disableStats: true,
   auth: {
     headers: {
-      Authorization: `Bearer ${BaseApi.defaults.headers.common?.Authorization || ''}`
-    }
+      Authorization: window.axios.defaults.headers.common["Authorization"] || "",
+    },
   },
 });
 
