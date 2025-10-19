38efd848d18670f674578da74f6357c2a9ca70ec
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-10-19 14:08:38 +0200

    Create MessageSent.php

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,24 @@
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    // broadcast on a channel
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+}
