6990cda79ad9593d35b88fc80306efb95b626d4c
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-10-20 13:14:53 +0200

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+
+Broadcast::channel('chat.{senderId}-{receiverId}', function ($user, $senderId, $receiverId) {
+    return $user->id === (int)$senderId || $user->id === (int)$receiverId;
+});
