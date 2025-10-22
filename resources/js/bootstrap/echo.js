b22f66be0c3c5c1c6c6fb8b95645984e1e99ef4a
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-10-22 10:09:06 +0200

    Update echo.js

--- resources/js/bootstrap/echo.js
+++ resources/js/bootstrap/echo.js
@@ -13,6 +13,7 @@ const echo = new Echo({
   disableStats: true,
   enabledTransports: ["ws", "wss"],
   authEndpoint: "/broadcasting/auth",
+cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
   auth: {
     headers: {
       "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]')?.getAttribute("content"),
