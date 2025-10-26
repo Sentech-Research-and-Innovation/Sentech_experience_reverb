960af50fa6721bb5cc2ab5f749d4974e8b89c244
MasimulaLawrence <152891372+MasimulaLawrence@users.noreply.github.com>
2025-10-26 04:04:45 +0200

    Update StoreNotificationTrait.php

--- app/Traits/StoreNotificationTrait.php
+++ app/Traits/StoreNotificationTrait.php
@@ -1,3 +1,50 @@
+// <?php
+
+// namespace App\Traits;
+
+// use Illuminate\Http\Request;
+// use App\Models\Notification;
+
+// trait StoreNotificationTrait
+// {
+
+
+//     public function StoreNotification($from_company_id, $notificationTypeId,  $sender = null)
+//     {
+
+//         $data = $this->getMessage($notificationTypeId);
+//         Notification::create([
+
+//             "model_ids" => [
+
+//                 "from_compay_id" => $from_company_id,
+//                 "to_compay_id" => 1,
+//                 "user_id" =>  $sender?->id ?? 0,
+
+//             ],
+//             "notification_type_id" => $notificationTypeId,
+//             "message" => $data['message'],
+//             "link" => $data["link"]
+//         ]);
+//     }
+
+
+//     private function getMessage($notificationTypeId, $sender = null)
+//     {
+//         $message = "";
+//         $link = "";
+//         if ($notificationTypeId == 1) {
+//             $message = "A new account has been requested";
+//             $link = "/organizantions/request";
+//         }elseif ($notificationTypeId == 2  && $sender) {
+//         $message = "{$sender->first_name} {$sender->last_name} sent you a text message";
+//         $link = "/profile/{$sender->id}";
+//     }
+//         return ["message" => $message, "link" => $link];
+//     }
+// }
+
+
 <?php
 
 namespace App\Traits;
@@ -7,39 +54,35 @@ use App\Models\Notification;
 
 trait StoreNotificationTrait
 {
-
-
-    public function StoreNotification($from_company_id, $notificationTypeId,  $sender = null)
+    public function StoreNotification($from_company_id, $notificationTypeId, $sender = null)
     {
+        $data = $this->getMessage($notificationTypeId, $sender);
 
-        $data = $this->getMessage($notificationTypeId);
         Notification::create([
-
             "model_ids" => [
-
-                "from_compay_id" => $from_company_id,
-                "to_compay_id" => 1,
-                "user_id" =>  $sender?->id ?? 0,
-
+                "from_company_id" => $from_company_id,
+                "to_company_id" => 1,
+                "user_id" => $sender?->id ?? 0,
             ],
             "notification_type_id" => $notificationTypeId,
             "message" => $data['message'],
-            "link" => $data["link"]
+            "link" => $data['link']
         ]);
     }
-
 
     private function getMessage($notificationTypeId, $sender = null)
     {
         $message = "";
         $link = "";
+
         if ($notificationTypeId == 1) {
             $message = "A new account has been requested";
-            $link = "/organizantions/request";
-        }elseif ($notificationTypeId == 2  && $sender) {
-        $message = "{$sender->first_name} {$sender->last_name} sent you a text message";
-        $link = "/profile/{$sender->id}";
-    }
+            $link = "/organizations/request";
+        } elseif ($notificationTypeId == 2 && $sender) {
+            $message = "{$sender->first_name} {$sender->last_name} sent you a text message";
+            $link = "/profile/{$sender->id}";
+        }
+
         return ["message" => $message, "link" => $link];
     }
 }
